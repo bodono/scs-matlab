8c5f5d8d409a43c57bd35c71b3a2eae5f9b84510
Alastair Abbott <al.a.abbott@gmail.com>
2021-03-04 20:12:47 +0100

    Update compile follows following scs refactoring

--- compile_indirect.m
+++ compile_indirect.m
@@ -2,8 +2,8 @@ function compile_indirect(flags, common_scs)
 
 % compile indirect
 if (flags.COMPILE_WITH_OPENMP)
-    cmd = sprintf('mex -O %s %s %s %s COMPFLAGS="/openmp \\$COMPFLAGS" CFLAGS="\\$CFLAGS -fopenmp" scs/linsys/indirect/private.c %s -Iscs -Iscs/include %s %s %s -output scs_indirect',  flags.arr, flags.LCFLAG, common_scs, flags.INCS, flags.link, flags.LOCS, flags.BLASLIB, flags.INT);
+    cmd = sprintf('mex -O %s %s %s %s COMPFLAGS="/openmp \\$COMPFLAGS" CFLAGS="\\$CFLAGS -fopenmp" scs/linsys/cpu/indirect/private.c %s -Iscs -Iscs/include %s %s %s -output scs_indirect',  flags.arr, flags.LCFLAG, common_scs, flags.INCS, flags.link, flags.LOCS, flags.BLASLIB, flags.INT);
 else
-    cmd = sprintf('mex -O %s %s %s %s scs/linsys/indirect/private.c %s -Iscs -Iscs/include -Iscs/linsys %s %s %s -output scs_indirect',  flags.arr, flags.LCFLAG, common_scs, flags.INCS, flags.link, flags.LOCS, flags.BLASLIB, flags.INT);
+    cmd = sprintf('mex -O %s %s %s %s scs/linsys/cpu/indirect/private.c %s -Iscs -Iscs/include -Iscs/linsys %s %s %s -output scs_indirect',  flags.arr, flags.LCFLAG, common_scs, flags.INCS, flags.link, flags.LOCS, flags.BLASLIB, flags.INT);
 end
 eval(cmd);
