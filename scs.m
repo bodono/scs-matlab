b065f16685dddff4b4e91e3d634ac5a01e670c78
Brendan O'Donoghue <bodonoghue85@gmail.com>
2021-11-26 12:32:59 +0000

    add some basic checks to input shapes

--- scs.m
+++ scs.m
@@ -20,6 +20,22 @@ if isfield(data, 'A')
     data.A = sparse(data.A);
 end
 
+if size(data.b, 2) > 1
+    data.b = data.b(:);
+end
+
+if size(data.c, 2) > 1
+    data.c = data.c(:);
+end
+
+assert(size(data.A, 1) == size(data.b, 1), "A and b shape mismatch")
+assert(size(data.A, 2) == size(data.c, 1), "A and c shape mismatch")
+
+if isfield(data, 'P')
+    assert(size(data.P, 1) == size(data.P, 2), "P is not square")
+    assert(size(data.P, 1) == size(data.c, 1), "P and c shape mismatch")
+end
+
 if (isfield(pars,'use_indirect') && pars.use_indirect)
     [  x, y, s, info  ] = scs_indirect( data, K, pars );
 elseif (isfield(pars,'gpu') && pars.gpu)
