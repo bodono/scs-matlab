9b1705f320bfd6735420ff7fd387a52ad05fbad3
Brendan O'Donoghue <bodonoghue85@gmail.com>
2023-03-22 14:26:51 +0000

    run
    clang-format -style="{BasedOnStyle: llvm, IndentWidth: 2, AllowShortFunctionsOnASingleLine: None, KeepEmptyLinesAtTheStartOfBlocks: false}" -i *.{c,h}

--- scs_version_mex.c
+++ scs_version_mex.c
@@ -2,13 +2,13 @@
 #include "scs.h"
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
-    /* matlab usage: ver = scs_version() */
-    if (nrhs != 0) {
-        mexErrMsgTxt("Too many input arguments.");
-    }
-    if (nlhs > 1) {
-        mexErrMsgTxt("Too many output arguments.");
-    }
-    plhs[0] = mxCreateString(scs_version());
-    return;
+  /* matlab usage: ver = scs_version() */
+  if (nrhs != 0) {
+    mexErrMsgTxt("Too many input arguments.");
+  }
+  if (nlhs > 1) {
+    mexErrMsgTxt("Too many output arguments.");
+  }
+  plhs[0] = mxCreateString(scs_version());
+  return;
 }
